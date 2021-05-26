91b35b4b01af80e896ef9bbc53367ba33ba45c9d
Kamran Haq <kamranhaq@protonmail.com>
2021-05-25 21:57:02 -0700

    Removed some default files. Signed-off-by: Kamran Haq <kamranhaq@protonmail.com>

--- src/main/java/com/physicsproject/App.java
+++ src/main/java/com/physicsproject/App.java
@@ -1,31 +1,22 @@
 package com.physicsproject;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
 
 
 public class App extends Application {
 
+    private Parent createContent() {
+        return new StackPane(new Text("Hello World"));
+    }
+
+
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-        ImageView imageView = new ImageView(new Image(App.class.getResourceAsStream("openduke.png")));
-        imageView.setFitHeight(200);
-        imageView.setPreserveRatio(true);
-
-        VBox root = new VBox(30, imageView, label);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 640, 480);
-        scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
-        stage.setScene(scene);
+        stage.setScene(new Scene(createContent(), 300, 300));
         stage.show();
     }
 
